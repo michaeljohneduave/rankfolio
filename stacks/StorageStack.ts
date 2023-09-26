a726ad662431868b8f41466fa187df16aa529a18
Michael Eduave <michaeljohneduave@gmail.com>
2023-09-26 13:57:17 -0700

    Remove test lighthouse script Add dynamodb for db storage

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,9 +1,24 @@
-import { Bucket, StackContext } from "sst/constructs";
+import { Bucket, StackContext, Table } from "sst/constructs";
 
 export function Storage({ stack }: StackContext) {
   const bucket = new Bucket(stack, "rankfolio-screenshot");
-
+  const table = new Table(stack, "folios", {
+    fields: {
+      url: "string",
+      name: "string",
+      latestScreenshot: "string",
+      latestLhReport: "string",
+      createdAt: "number",
+      updatedAt: "number",
+      upVotes: "number",
+      downVotes: "number",
+    },
+    primaryIndex: {
+      partitionKey: "url",
+    },
+  });
   return {
     bucket,
+    table,
   };
 }
