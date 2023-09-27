fb30d66f20e68dff2cf87d01f3040258018df3d0
Michael Eduave <michaeljohneduave@gmail.com>
2023-09-27 11:55:50 -0700

    Refactor main lambda into sqs consumer

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,14 +1,115 @@
-import puppeteer from "puppeteer-core";
+import puppeteer, { Page } from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 import * as aws from "aws-sdk";
 import lighthouse from "lighthouse";
-import handler from "@rankfolio/core/handler";
 import { Bucket } from "sst/node/bucket";
+import { SQSEvent } from "aws-lambda";
+import { Table } from "sst/node/table";
 
 const LOCAL_CHROMIUM_PATH = "/opt/homebrew/bin/chromium";
+const dynamoDb = new aws.DynamoDB.DocumentClient();
+const s3 = new aws.S3();
 
-export const main = handler<string>(async (_evt) => {
-  const { url } = _evt.queryStringParameters!;
+const runScreenshot = async (page: Page) => {
+  let screenshot;
+  let screenshotUrl;
+  let error;
+  let retries = 0;
+
+  do {
+    retries += 1;
+    try {
+      screenshot = (await page.screenshot({ fullPage: false })) as Buffer;
+    } catch (e) {
+      error = e instanceof Error ? e.message : JSON.stringify(e);
+      console.error(error);
+      console.error("Retrying screenshot...");
+    }
+  } while (retries < 3 && !screenshot);
+
+  if (screenshot) {
+    try {
+      const screenshotName = `${Date.now()}-${page.url()}-screenshot.png`;
+      await s3
+        .putObject({
+          Bucket: Bucket["rankfolio-screenshot"].bucketName,
+          Key: screenshotName,
+          Body: screenshot,
+          ContentType: "image/png",
+          ACL: "public-read",
+        })
+        .promise();
+      screenshotUrl = `https://${Bucket["rankfolio-screenshot"].bucketName}.s3.amazonaws.com/${screenshotName}`;
+    } catch (e) {
+      error = e instanceof Error ? e.message : JSON.stringify(e);
+      console.error(error);
+    }
+  }
+
+  return {
+    data: screenshotUrl,
+    error,
+  };
+};
+
+const runLigthhouse = async (url: string, page: Page) => {
+  let result;
+  let retries;
+  let reportUrl;
+  let error;
+
+  retries = 0;
+
+  do {
+    retries += 1;
+    try {
+      result = await lighthouse(
+        url,
+        {
+          output: "html",
+        },
+        undefined,
+        page
+      );
+    } catch (e) {
+      error = e instanceof Error ? e.message : JSON.stringify(e);
+      console.error(error);
+      console.error(url, "Retrying lighthouse...");
+    }
+  } while (retries < 3 && !result);
+
+  if (result) {
+    try {
+      const reportName = `${Date.now()}-${encodeURI(url)}-report.html`;
+      await s3
+        .putObject({
+          Bucket: Bucket["rankfolio-screenshot"].bucketName,
+          Key: reportName,
+          Body: result?.report,
+          ContentType: "text/html",
+          ACL: "public-read",
+        })
+        .promise();
+      reportUrl = `https://${Bucket["rankfolio-screenshot"].bucketName}.s3.amazonaws.com/${reportName}`;
+    } catch (e) {
+      error = e instanceof Error ? e.message : JSON.stringify(e);
+      console.error(e);
+    }
+  }
+
+  return {
+    data: {
+      url: reportUrl,
+      report: result?.lhr,
+    },
+    error,
+  };
+};
+
+export const main = async (event: SQSEvent) => {
+  const body = JSON.parse(event.Records[0].body);
+  const url = body.payload.website;
+  const name = body.payload.name;
   const removeArgs = ["--single-process"];
   const args = chromium.args;
   if (!url) throw new Error("url is required");
@@ -31,81 +132,52 @@ export const main = handler<string>(async (_evt) => {
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
 
-  let retries = 0;
+  const { data: screenshot, error: screenshotError } = await runScreenshot(
+    page
+  );
+  const { data: result, error: lighthouseError } = await runLigthhouse(
+    url,
+    page
+  );
 
-  let screenshot;
-  let result;
+  try {
+    const pages = await browser.pages();
+    await Promise.all(pages.map((page) => page.close()));
+    await browser.close();
+  } catch (e) {
+    console.error(e);
+    console.error("Error closing pages/browser");
+  }
 
-  do {
-    retries += 1;
-    try {
-      screenshot = (await page.screenshot({ fullPage: false })) as Buffer;
-    } catch (e) {
-      console.error(e);
-      console.error("Retrying...");
-    }
-  } while (retries < 3 && !result);
-
-  retries = 0;
-
-  do {
-    retries += 1;
-    try {
-      result = await lighthouse(
-        url,
-        {
-          output: "html",
-        },
-        undefined,
-        page
-      );
-    } catch (e) {
-      console.error(e);
-      console.error("Retrying...");
-    }
-  } while (retries < 3 && !result);
-
-  if (!screenshot || !result) throw new Error("Failed to take screenshot");
-
-  const pages = await browser.pages();
-  await Promise.all(pages.map((page) => page.close()));
-  await browser.close();
-
-  const s3 = new aws.S3();
-  const screenshotName = `${Date.now()}-screenshot.png`;
-  const reportName = `${Date.now()}-report.html`;
-
-  await Promise.all([
-    s3
-      .putObject({
-        Bucket: Bucket["rankfolio-screenshot"].bucketName,
-        Key: screenshotName,
-        Body: screenshot,
-        ContentType: "image/png",
-        ACL: "public-read",
-      })
-      .promise(),
-
-    s3
-      .putObject({
-        Bucket: Bucket["rankfolio-screenshot"].bucketName,
-        Key: reportName,
-        Body: result?.report,
-        ContentType: "text/html",
-        ACL: "public-read",
-      })
-      .promise(),
-  ]);
-
-  return JSON.stringify({
-    result: {
-      screenshot: `https://${Bucket["rankfolio-screenshot"].bucketName}.s3.amazonaws.com/${screenshotName}`,
-      html: `https://${Bucket["rankfolio-screenshot"].bucketName}.s3.amazonaws.com/${reportName}`,
-      performance: result?.lhr.categories.performance.score,
-      accessibility: result?.lhr.categories.accessibility.score,
-      "best-practices": result?.lhr.categories["best-practices"].score,
-      seo: result?.lhr.categories.seo.score,
-      pwa: result?.lhr.categories.pwa.score,
+  const params = {
+    TableName: Table["folios"].tableName,
+    Item: {
+      url: url,
+      name: name,
+      latestScreenshot: "",
+      latestLhUrl: "",
+      latestLhData: "",
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      upVotes: 0,
+      downVotes: 0,
     },
-  });
-});
+  };
+
+  if (screenshot) {
+    params.Item.latestScreenshot = screenshot;
+  } else {
+    console.error(screenshotError);
+  }
+
+  if (result.report) {
+    params.Item.latestLhUrl = result?.url || "";
+    params.Item.latestLhData = JSON.stringify(result.report);
+  } else {
+    console.error(lighthouseError);
+  }
+
+  await dynamoDb.put(params).promise();
+
+  return {};
+};
